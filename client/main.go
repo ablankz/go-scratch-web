aa9558300f9fa25fbab61e4c5bf70d73ab6adf5c
blank <hayaken3007@i.softbank.jp>
2023-12-04 14:57:03 +0900

    keep aliveに対応

--- client/main.go
+++ client/main.go
@@ -12,9 +12,14 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	RequestAndResponse(conn, "request0")
+	RequestAndResponse(conn, "request1")
+	RequestAndResponse(conn, "request2")
 
-	requestData := "request"
+	RequestAndResponse(conn, "close")
+}
 
+func RequestAndResponse(conn net.Conn, requestData string) {
 	requestByteData, err := json.Marshal(requestData)
 	if err != nil {
 		fmt.Println(err)
--- main.go
+++ main.go
@@ -20,39 +20,47 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(conn)
+		fmt.Println("request accepted...")
 
 		go handle(conn)
 	}
 }
 
 func handle(conn net.Conn) {
-	buf := make([]byte, 1000)
+	for {
+		buf := make([]byte, 1000)
 
-	// requestを受け取る, nは長さ
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println(err)
-		return
+		// requestを受け取る, nは長さ
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		requestInfo := string(buf[:n])
+
+		fmt.Println("request:")
+		fmt.Println(requestInfo)
+
+		if requestInfo == `"close"` {
+			fmt.Println("closing from client...")
+			conn.Close()
+			return
+		}
+
+		responseData := "response"
+
+		responseByteData, err := json.Marshal(responseData)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// レスポンス返却
+		_, err = conn.Write(responseByteData)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
-
-	fmt.Println("request:")
-	fmt.Println(string(buf[:n]))
-
-	responseData := "response"
-
-	responseByteData, err := json.Marshal(responseData)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// レスポンス返却
-	_, err = conn.Write(responseByteData)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	conn.Close()
 }
