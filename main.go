63ea8ef1c74da70679dcc1c6173b8d38076c1884
blank <hayaken3007@i.softbank.jp>
2023-12-04 14:27:59 +0900

    サーバーのリスナを立ち上げる

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	handle(conn)
+}
+
+func handle(conn net.Conn) {
+	buf := make([]byte, 1000)
+
+	// requestを受け取る, nは長さ
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("request:")
+	fmt.Println(buf[:n])
+
+	responseData := "response"
+
+	responseByteData, err := json.Marshal(responseData)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// レスポンス返却
+	_, err = conn.Write(responseByteData)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn.Close()
+}
